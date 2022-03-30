1b7884ec6fd3a5e72147354fc96c6382ab5254b5
Omar Ashour <omar.a.ashour@gmail.com>
2022-03-30 13:47:56 +0200

    Added js for empty input error

--- dist/script.js
+++ dist/script.js
@@ -1,2 +1,43 @@
 // Elements
 const submitBtnEL = document.querySelector('.submit-btn');
+const inputsArr = document.querySelectorAll('input');
+
+
+// Event Listeners
+submitBtnEL.addEventListener('click', checkInfo);
+
+function checkInfo(e) {
+  inputsArr.forEach(inputEL => {
+    if (inputEL.value === '') {
+      inputEL.previousElementSibling.style.display = 'block';
+      inputEL.classList.add('input-error');
+      const errTextEL = document.createElement('small');
+      errTextEL.className = 'error-text';
+      errTextEL.textContent = `${inputEL.getAttribute('placeholder')} cannot be empty`;
+      inputEL.parentElement.appendChild(errTextEL);
+    }  
+  });
+
+  inputsArr.forEach(inputEL => inputEL.addEventListener('focus', removeErr));
+  
+  const errMsgArr = document.querySelectorAll('.error-text');
+
+  function removeErr(){
+    errMsgArr.forEach(errMsgEL => errMsgEL.remove());
+
+    inputsArr.forEach(inputEL => { 
+      inputEL.previousElementSibling.style.display = 'none';
+      inputEL.classList.remove('input-error');
+    });
+  }; 
+  e.preventDefault();
+}
+
+
+ 
+
+
+
+// dynamically insert error classes and elements based on if inputs are valid or not
+// work on better email regex
+// use onfocus to toggle classes, display/hide elements
