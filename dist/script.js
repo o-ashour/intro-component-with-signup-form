52c5a7fc84b0e3a27a7cd714720f0fbd53473165
Omar Ashour <omar.a.ashour@gmail.com>
2022-03-30 20:12:29 +0200

    Added grid layout for larger sizes

--- dist/script.js
+++ dist/script.js
@@ -1,6 +1,9 @@
 // Elements
 const submitBtnEL = document.querySelector('.submit-btn');
 const inputsArr = document.querySelectorAll('input');
+const emailEL = document.getElementById('email');
+
+let mailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
 
 // Event Listeners
@@ -9,14 +12,25 @@ submitBtnEL.addEventListener('click', checkInfo);
 function checkInfo(e) {
   inputsArr.forEach(inputEL => {
     if (inputEL.value === '') {
+      submitBtnEL.setAttribute('disabled', '');
       inputEL.previousElementSibling.style.display = 'block';
       inputEL.classList.add('input-error');
       const errTextEL = document.createElement('small');
       errTextEL.className = 'error-text';
       errTextEL.textContent = `${inputEL.getAttribute('placeholder')} cannot be empty`;
       inputEL.parentElement.appendChild(errTextEL);
-    }  
+    }
   });
+  // console.log(emailEL.value);
+  if (emailEL.value !== '' && !mailRegex.test(emailEL.value)) {
+    submitBtnEL.setAttribute('disabled', '');
+    emailEL.previousElementSibling.style.display = 'block';
+    emailEL.classList.add('input-error');
+    const mailErrMsgEL = document.createElement('small');
+    mailErrMsgEL.className = 'error-text';
+    mailErrMsgEL.textContent = 'Looks like this is not an email';
+    emailEL.parentElement.appendChild(mailErrMsgEL);
+  }
 
   inputsArr.forEach(inputEL => inputEL.addEventListener('focus', removeErr));
   
@@ -29,15 +43,8 @@ function checkInfo(e) {
       inputEL.previousElementSibling.style.display = 'none';
       inputEL.classList.remove('input-error');
     });
+
+    submitBtnEL.removeAttribute('disabled');
   }; 
   e.preventDefault();
 }
-
-
- 
-
-
-
-// dynamically insert error classes and elements based on if inputs are valid or not
-// work on better email regex
-// use onfocus to toggle classes, display/hide elements
